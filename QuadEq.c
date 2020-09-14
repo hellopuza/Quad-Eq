d73734a2ed190b54ab3683133571749ce55082b3
hellopuza <71190274+hellopuza@users.noreply.github.com>
2020-09-14 19:35:16 +0300

    Update QuadEq.c

--- QuadEq.c
+++ QuadEq.c
@@ -1,3 +1,13 @@
+/*------------------------------------------------------------------------------
+     * File: QuadEq.c
+     * Description: Program solves quadratic equations.
+     * Created: 14 sep 2020
+     * Copyright: (C) 2020 MIPT
+     * Author: Artem Puzankov
+     * Email: puzankov.ao@phystech.edu
+     */
+
+
 #include <stdio.h>
 #include <math.h>
 
@@ -9,17 +19,30 @@ bool IsZero (double value);
 void GetCoef(char c, double* value);
 double StrtoFlo(char *str);
 int Getline(char* line);
+int TestEq(double a, double b, double c, int num, double root1, double root2);
 
 
-const int NumbLen = 12;
-const double nil = 1e-7;
-const int noRoots = -1;
+const int NumbLen  = 12;
+const double nil   = 1e-7;
+const int noRoots  = -1;
 const int infRoots = 3;
 
 //------------------------------------------------------------------------------
 
 int main()
 {
+// Tests ----------------------------------------------------------------------
+//>
+	TestEq(3, 4, 2, noRoots, 0, 0);
+	TestEq(5, 8, -9, 2, 0.762049935, -2.362049935);
+	TestEq(1, 0, -4, 2, -2, 2);
+	TestEq(1, 6, 9, 1, -3, 0);
+//>
+
+/*!
+ *
+ */
+
     double a = 0;
     double b = 0;
     double c = 0;
@@ -66,6 +89,12 @@ int main()
 
 int SolveEq(double a, double b, double c, double *root1, double *root2)
 {
+
+/*!
+ * Assign an equation type (linear or quadratic)
+ * return number of roots
+ */
+
     int NumbRoots = 0;
 
     if (IsZero(a))
@@ -80,6 +109,13 @@ int SolveEq(double a, double b, double c, double *root1, double *root2)
 
 int SolveQuadEq(double a, double b, double c, double *root1, double *root2)
 {
+
+/*!
+ * Solves quadratic equation a*x*x + b*x + c = 0
+ * return number of roots
+ * a, b, c - coefficients
+ */
+
     double discr = b*b - 4*a*c;
 
     if (IsZero(discr)) {
@@ -104,6 +140,13 @@ int SolveQuadEq(double a, double b, double c, double *root1, double *root2)
 
 int SolveLinEq(double a, double b, double *root)
 {
+
+/*!
+ * Solves linear equation a*x + b = 0
+ * return number of roots
+ * a, b, - coefficients
+ */
+
     if (IsZero(a) && IsZero(b)) return infRoots;
 
     else if (IsZero(a)) return noRoots;
@@ -121,6 +164,12 @@ int SolveLinEq(double a, double b, double *root)
 
 bool IsZero (double value)
 {
+
+/*!
+ * return true if value is zero
+ * return false if value is not zero
+ */
+
     if ((value <= nil) && (value >= - nil)) return true;
 
     else return false;
@@ -130,6 +179,12 @@ bool IsZero (double value)
 
 void GetCoef(char c, double* value)
 {
+
+/*!
+ * Checks that get line (str) is number
+ * puts number to value using StrtoFlo()
+ */
+
     char str[NumbLen] {'\0'};
     int error = 0;
 
@@ -196,6 +251,12 @@ void GetCoef(char c, double* value)
 
 double StrtoFlo(char *str)
 {
+
+/*!
+ * Сonverts a string to a signed double number
+ * return number
+ */
+
     double numb = 0;
     int sign = 1;
     if (*str == '-') {
@@ -229,6 +290,12 @@ double StrtoFlo(char *str)
 
 int Getline(char* line)
 {
+
+/*!
+ * get string line from input
+ * return length of line
+ */
+
     int c, i;
 
     for (i = 0; (i < NumbLen - 1) && ((c = getchar()) != '\n'); ++i)
@@ -237,6 +304,42 @@ int Getline(char* line)
     if (c == '\n') line[i] = '\0';
 
     return i;
+}
+
+//------------------------------------------------------------------------------
+
+int TestEq(double a, double b, double c, int num, double root1, double root2)
+{
+
+/*!
+ * doing test of SolveEq()
+ */
+
+	double gotRoot1 = 0;
+	double gotRoot2 = 0;
+
+	int result = SolveEq(a, b, c, &gotRoot1, &gotRoot2);
+
+	if ( (result == num) && (IsZero(root1 - gotRoot1)
+                         &&  IsZero(root2 - gotRoot2))
+                         || (IsZero(root1 - gotRoot2)
+                         &&  IsZero(root2 - gotRoot1)) ) {
+
+		printf("\n Test %lf, %lf, %lf is SUCCESSFUL\n", a, b, c);
+
+		return 0;
+
+    } else {
+
+		printf("\n Test %lf, %lf, %lf is FAILED\n"
+
+               "\n Expected num = %d, root1 = %lg, root2 = %lg\n"
+
+		 	   "\n Got num = %d, root1 = %lg, root2 = %lg\n",
+
+			    a, b, c, num, root1, root2, result, gotRoot1, gotRoot2);
+		return 1;
+    }
 }
 
 //------------------------------------------------------------------------------
